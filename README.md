d8e8279dac56c1ddc3891d196f3ee807b7543ae9
Makreig <mark@i-lateral.com>
2019-09-23 15:50:57 +0100

    added core upgreade task

--- README.md
+++ README.md
@@ -1,29 +1,17 @@
 # upgrader
 Upgrade legacy silverstripe-commerce (SS3) sites to SilverCommerce
 
+# !!! Note: This is the SS4 branch - you will need to start on the SS3 branch !!! 
+
 ## How it works
 
 To use this module, you need to install it before running the silverstripe upgrade tool.
 By doing so, this will swap out the old i-lateral/silverstripe-[module] commerce modules for the silvercommerce/[module] modules when the silverstripe upgrader is ran.
 
-### Intsalling
-```
-composer require silvercommerce/upgrader
-```
+### Installing
+
+#### See SS3 branch
+
 ### Usage
-Once installed you needd to remove any/all of the followinng modules from your composer.json
+This module provides a `CommerceUpgradeTask` you can run in the final steps of a standard SS3 to SS4 upgrade. This will update all your old DB objects into the new types.
 
-```
-  i-lateral/silverstripe-commerce
-  i-lateral/silverstripe-orders
-  i-lateral/silverstripe-commerce-customisableproduct
-  i-lateral/silverstripe-commerce-bulkprice
-  silverstripe/silverstripe-omnipay
-  i-lateral/silverstripe-commerce-groupedproduct
-  i-lateral/silverstripe-themes-kube-commerce
-```
-then run `composer update` to update your composer.lock and to ensure you have the latest versions of all the modules.
-
-### Now the hard bit
-
-now you need to work through all of the errors you're going to get: *This module is not yet finished*
--- tasks/CommerceUpgradeTask.php
+++ tasks/CommerceUpgradeTask.php
@@ -1,16 +1,111 @@
 <?php
 
 use SilverStripe\Dev\BuildTask;
+use SilverCommerce\OrdersAdmin\Model\Invoice;
+use SilverCommerce\OrdersAdmin\Model\Estimate;
+use SilverCommerce\OrdersAdmin\Model\LineItem;
+use SilverStripe\ORM\Queries\SQLDelete;
+use SilverStripe\ORM\Queries\SQLSelect;
 
+/**
+ * Task to update old commerce orders into new invoices
+ */
 class CommerceUpgradeTask extends BuildTask
 {
-    protected $title = 'Say Hi';
+    protected $title = 'MigrateCommerce';
  
-    protected $description = 'A class that says <strong>Hi</strong>';
+    protected $description = 'Migrate SS3 Commerce DB to SilverCommerce';
  
     protected $enabled = true;
- 
+
     function run($request) {
-        echo "I'm trying to say hi...";
-    }   
+
+        $this->convertOrders();
+        $this->convertItems();
+    }  
+    
+    /**
+     * Convert Orders/Estimates to Estimates/Invoices
+     *
+     * @return void
+     */
+    private function convertOrders() {
+        $query = new SQLSelect();
+        $query->setFrom('"Order"');
+
+        $result = $query->execute();
+
+        foreach ($result as $row) {
+            $id = $row['ID'];
+            $row = array_filter($row);
+            $fetch = SQLSelect::create()
+                ->setFrom('"Estimate"')
+                ->setWhere(array('"Estimate"."ID"' => $id));
+            $est = $fetch->execute()->first();
+            $est = array_filter($est);
+            $row = array_merge($est, $row);
+
+            ### Update object data
+            foreach ($row as $key => $value)
+            {
+            }
+
+            ### Apply changes to database
+            if ($row['ClassName'] == 'Estimate') {
+                $existing = Estimate::get()->byID($row['ID']);
+                // if an estimate already exists in the system we should update it instead of creating a new one
+                $row['ClassName'] = Estimate::class;
+                if ($existing) {
+                    $existing->update($row);
+                    $existing->write();
+                } else {
+                    $item = Estimate::create()->update($row);
+                    $item->write();
+                }
+            } else {
+                $existing = Estimate::get()->byID($row['ID']);
+
+                $row['ClassName'] = Invoice::class;
+                if ($existing) {
+                    $existing->update($row);
+                    $existing->write();
+                } else {
+                    $item = Invoice::create()->update($row);
+                    $item->write();
+                }
+            }
+        }
+    }
+
+    /**
+     * convert OrderItems to LineItems & their Customisations
+     *
+     * @return void
+     */
+    private function convertItems()
+    {
+        $query = new SQLSelect();
+        $query->setFrom('"OrderItem"');
+
+        $result = $query->execute();
+
+        foreach ($result as $row) {
+            $id = $row['ID'];
+            ### Update object data
+            foreach ($row as $key => $value)
+            {
+            }
+
+            ### Apply changes to database
+            $existing = LineItem::get()->byID($row['ID']);
+            $row['ClassName'] = LineItem::class;
+            if ($existing) {
+                $existing->update($row);
+                $existing->write();
+            } else {            
+                $item = LineItem::create()->update($row);
+                $item->write();
+            }
+        }
+    }
 }
